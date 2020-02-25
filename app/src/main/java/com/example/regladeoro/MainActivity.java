e7648f33bbe118f8a97551c2a4067df16f80fec4
Juan Sebastian Cubillos Fuentes <1001089317@academico.icesi.edu.co>
2020-02-25 12:57:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/regladeoro/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.regladeoro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private TextView contador;
+
+    int c = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contador = findViewById(R.id.Contador);
+
+        new Thread(
+                ()->{
+
+                    while(true){
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        c++;
+
+
+
+                        runOnUiThread(
+                                ()->{
+                                    contador.setText(""+c);
+                                }
+                        );
+
+                    }
+                }
+        ).start();
+    }
+}
